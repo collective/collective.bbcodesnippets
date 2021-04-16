3e4a6091409e52c66695807fb645be121bc0825d
Michael Graf <michael.graf@fhstp.ac.at>
2021-04-16 17:05:09 +0200

    chore: make tinymce button visible

--- js/src/main.js
+++ js/src/main.js
@@ -1,77 +1,33 @@
-import { list } from "./restapi.js";
+require(["jquery", "pat-registry", "tinymce"], function ($, Registry, tinymce) {
+  "use strict";
+  console.log("works 1");
+  var portalUrl = $("body").attr("data-portal-url");
+  const buttonIcon =
+    portalUrl + "/++plone++collective.bbcodesnippets/bbcodeicon.png";
+  console.log("create and add collectivebbcodesnippets");
+  var tinymce = require("tinymce");
+  tinymce.create("tinymce.plugins.CollectiveBBCodeSnippetsPlugin", {
+    init: function (editor) {
+      editor.on("init", function () {
+        // make all existing not editable
+        $('[data-type="snippet_tag"]', editor.getBody()).each(function () {
+          this.setAttribute("contenteditable", false);
+        });
+      });
 
-require(["tinymce"], function (tinymce) {
-  console.log("create and add collectivebbcodesnippets")
+      editor.addCommand("collectivebbcodesnippets", function () {
+        var $el = $(editor.selection.getNode());
+        console.log("works!");
+      });
 
-  tinymce.PluginManager.add('collectivebbcodesnippets', function(editor, url) {
-
-    console.log(editor)
-    console.log(url)
-    const portalUrl = $("body").attr("data-portal-url")
-    const buttonIcon = portalUrl + "/++plone++collective.bbcodesnippets/bbcodeicon.png"
-    list()
-    .then((datalist) => {
-      console.log(datalist)
-      datalist.forEach( (entry, index) => {
-        const name = 'bbcs_' + entry.name
-        console.log(entry.name)
-        editor.addMenuItem(name, {
-          text: entry.name,
-          onAction: () => {
-            alert(entry.snippet)
-          }
-        })
-      })
-    })
-    .catch( (err) => {
-      console.log(err)
-    })
-
-    /* Return the metadata for the help plugin */
-    return {
-      getMetadata: function () {
-        return  {
-          name: 'collective.bbcodesnippets plugin',
-          url: 'https://github.com/collective/collective.bbcodesnippets'
-        };
-      }
-    };
+      editor.addButton("cbbcodesnippetsbutton", {
+        cmd: "collectivebbcodesnippets",
+        image: buttonIcon,
+      });
+    },
   });
-  
-
-  // tinymce.PluginManager.add("collectivebbcodesnippets", (editor, url) => {
-  //   console.log(editor)
-  //   console.log(url)
-  //   const portalUrl = $("body").attr("data-portal-url")
-  //   const buttonIcon = portalUrl + "/++plone++collective.bbcodesnippets/bbcodeicon.png"
-  //   list()
-  //   .then((datalist) => {
-  //     console.log(datalist)
-  //     datalist.forEach( (entry, index) => {
-  //       const name = 'bbcs_' + entry.name
-  //       console.log(entry.name)
-  //       editor.ui.registry.addMenuItem(name, {
-  //         text: entry.name,
-  //         onAction: () => {
-  //           alert(entry.snippet)
-  //         }
-  //       })
-  //     })
-  //   })
-  //   .catch( (err) => {
-  //     console.log(err)
-  //   })
-  //   return {
-  //     init: (editor) => {
-  //       console.log("init collectivebbcodesnippets")
-  //     },
-  //     getMetadata: function () {
-  //       return  {
-  //         name: 'https://github.com/collective/collective.bbcodesnippets',
-  //         url: 'https://github.com/collective/collective.bbcodesnippets'
-  //       };
-  //     }      
-  //   }
-  // })
-
-})()
+  tinymce.PluginManager.add(
+    "collectivebbcodesnippets",
+    tinymce.plugins.CollectiveBBCodeSnippetsPlugin
+  );
+});
